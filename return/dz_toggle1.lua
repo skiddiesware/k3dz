ec76be509311ba920b90ad7d34e02a57ecbee1d1
skiddiesware <tototpetot745@gmail.com>
2025-05-06 14:02:34 +0700

    Create dz_toggle1.lua

---
+++ return/dz_toggle1.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        TabKey  = "Main",
+        Name    = "AutoFlashback",    -- an internal ID
+        Title   = "Auto-Flashback",
+        Default = false,
+        OnChanged = function(v)
+            -- your existing handler logic here …
+        end
+    },
+    -- add more toggles as needed
+}
