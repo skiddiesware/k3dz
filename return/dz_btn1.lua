8d0c97f63fff5e3b657a7d4457fa01e4ce05eb93
skiddiesware <tototpetot745@gmail.com>
2025-05-06 13:58:49 +0700

    Update and rename dz_btn1 to dz_btn1.lua

---
+++ return/dz_btn1.lua
@@ -0,0 +1,51 @@
+return {
+    {
+        Title = "Infinite Yield",
+        Description = "IY",
+        Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+        end
+    },
+    {
+        Title = "LALOL",
+        Description = "BACKDOOR",
+        Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/Backdoor-Scanner/script"))()
+        end
+    },
+    {
+        Title = "MICRO",
+        Description = "BACKDOOR",
+        Callback = function()
+            loadstring(game:HttpGet("https://paste.ee/r/GipgWZb8"))()
+        end
+    },
+    {
+        Title = "SolSpy",
+        Description = "alternative of remoteSpy",
+        Callback = function()
+            loadstring(game:HttpGet("https://gist.githubusercontent.com/Uylost/960b592d7fc7161c3e6de0047547d874/raw/cc0e9ca436afe2c3e253fdb357d1a61efd576098/gistfile1.txt"))()
+        end
+    },
+    {
+        Title = "freaky R6 Animations",
+        Description = "freak",
+        Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/ShutUpJamesTheLoser/freaky/refs/heads/main/fe"))()
+        end
+    },
+    {
+        Title = "FE R6 Animations",
+        Description = "this one focus more on gameplay",
+        Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/redxs-dosx/R6-Anim-Hub/refs/heads/main/Hub%20Script%20(.lua)"))()
+        end
+    },
+    {
+        Title = "Aqua FE R6 Animations",
+        Description = "this is the best animation script with a ton of animations",
+        Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/ExploitFin/AquaMatrix/refs/heads/AquaMatrix/AquaMatrix"))()
+        end
+    }
+}
