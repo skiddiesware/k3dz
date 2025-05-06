edc315ffe69c6a42b3f52603b3b3bb1291bf4fde
skiddiesware <tototpetot745@gmail.com>
2025-05-06 15:36:05 +0700

    Update dz_tab1.lua

--- return/dz_tab1.lua
+++ return/dz_tab1.lua
@@ -1,5 +1,9 @@
 return {
     Main = { Title = "Universal", Icon = "" },
-    Storage = { Title = "Storage", Icon = "archive" },
+    stk = { Title = "Survive The Killer", Icon = "" },
+    deadrail = { Title = "Dead Rails", Icon = "" },
+    garden = { Title = "Grow A Garden", Icon = "" },
+    lt2 = { Title = "Lumber Tycoon 2", Icon = "" },
+    blrivals = { Title = "Blue Lock: RIVALS", Icon = "" },
     Settings = { Title = "Fluent", Icon = "" }
 }
