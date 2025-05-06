42e8f50bd98e4a6cd736ec70bee416b08dc8310e
skiddiesware <tototpetot745@gmail.com>
2025-05-06 14:14:46 +0700

    Update dz_btn1.lua

--- return/dz_btn1.lua
+++ return/dz_btn1.lua
@@ -1,6 +1,7 @@
 return {
     {
         Title = "Infinite Yield",
+        TabKey = "Main",
         Description = "IY",
         Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
@@ -8,6 +9,7 @@ return {
     },
     {
         Title = "LALOL",
+        TabKey = "Main",
         Description = "BACKDOOR",
         Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/Backdoor-Scanner/script"))()
@@ -15,6 +17,7 @@ return {
     },
     {
         Title = "MICRO",
+        TabKey = "Main",
         Description = "BACKDOOR",
         Callback = function()
             loadstring(game:HttpGet("https://paste.ee/r/GipgWZb8"))()
@@ -22,6 +25,7 @@ return {
     },
     {
         Title = "SolSpy",
+        TabKey = "Main",
         Description = "alternative of remoteSpy",
         Callback = function()
             loadstring(game:HttpGet("https://gist.githubusercontent.com/Uylost/960b592d7fc7161c3e6de0047547d874/raw/cc0e9ca436afe2c3e253fdb357d1a61efd576098/gistfile1.txt"))()
@@ -29,6 +33,7 @@ return {
     },
     {
         Title = "freaky R6 Animations",
+        TabKey = "Main",
         Description = "freak",
         Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/ShutUpJamesTheLoser/freaky/refs/heads/main/fe"))()
@@ -36,6 +41,7 @@ return {
     },
     {
         Title = "FE R6 Animations",
+        TabKey = "Main",
         Description = "this one focus more on gameplay",
         Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/redxs-dosx/R6-Anim-Hub/refs/heads/main/Hub%20Script%20(.lua)"))()
@@ -43,6 +49,7 @@ return {
     },
     {
         Title = "Aqua FE R6 Animations",
+        TabKey = "Main",
         Description = "this is the best animation script with a ton of animations",
         Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/ExploitFin/AquaMatrix/refs/heads/AquaMatrix/AquaMatrix"))()
